185a7c8254ee7fc9ca64961937f0541c06c6794d
Corey Tegeler <coreytegeler@gmail.com>
2024-06-26 10:03:44 -0400

    Strip to placeholder

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,34 +1,7 @@
 <script>
-	import { onMount } from 'svelte'
 	import '$src/globals.css'
 	import '$src/styles.css'
 	import Hero from '$lib/Hero.svelte'
-	import Header from '$lib/Header.svelte'
-	import Footer from '$lib/Footer.svelte'
-
-	let main
-	let header
-	let y = 0
-	let fillHeader = false
-	$: onScroll(y);
-
-	const onScroll = () => {
-		const mainTop = main ? main?.getBoundingClientRect()?.top : null
-		const headerHeight = header ? header?.getBoundingClientRect()?.height : null
-		fillHeader = mainTop < headerHeight + 10
-	}
 </script>
 
-<svelte:window bind:scrollY={y} />
-<div id="home" />
-<Header
-	bind:header
-	fill={fillHeader}
-/>
 <Hero />
-<main
-	bind:this={main}
->
-	<slot />
-</main>
-<Footer />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,1 @@
-<script>
-	import About from '$lib/sections/About.svelte'
-	// import Journalism from '$lib/sections/Journalism.svelte'
-	// import HumanRights from '$lib/sections/HumanRights.svelte'
-	import Values from '$lib/sections/Values.svelte'
-	import Team from '$lib/sections/Team.svelte'
-	import Contact from '$lib/sections/Contact.svelte'
-</script>
 
-<About />
-<Values />
-<Team />
-<Contact />
