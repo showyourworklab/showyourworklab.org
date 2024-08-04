3a71281b63249225f2e724461cd24671d7da686c
Corey Tegeler <coreytegeler@gmail.com>
2024-08-04 12:01:57 -0400

    Adjust hero shadow

--- .svelte-kit/output/server/manifest-full.js
+++ .svelte-kit/output/server/manifest-full.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set([".nojekyll","images/favicon-16x16.png","images/favicon-32x32.png","images/favicon.ico","images/icon.png","images/logo-light.svg","images/logo-sm-light.svg","images/social.png","photos/photo_1.jpg","photos/photo_2.jpg","photos/photo_3.jpg","photos/photo_4.jpg","photos/photo_5.jpg","photos/photo_6.jpg","photos/photo_7.jpg","photos/photo_8.jpg","photos/photo_9.jpg","photos/syw.jpg","photos/syw_corey.jpg","photos/syw_nora.jpg"]),
 	mimeTypes: {".png":"image/png",".svg":"image/svg+xml",".jpg":"image/jpeg"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.DinXUgLa.js","app":"_app/immutable/entry/app.Chij60m3.js","imports":["_app/immutable/entry/start.DinXUgLa.js","_app/immutable/chunks/entry.DDGT2nh9.js","_app/immutable/chunks/scheduler.BBaljRlA.js","_app/immutable/entry/app.Chij60m3.js","_app/immutable/chunks/scheduler.BBaljRlA.js","_app/immutable/chunks/index.nkUJBPft.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"_app/immutable/entry/start.C1RUJSDG.js","app":"_app/immutable/entry/app.1zOPkDX9.js","imports":["_app/immutable/entry/start.C1RUJSDG.js","_app/immutable/chunks/entry.DEwXoSDo.js","_app/immutable/chunks/scheduler.BBaljRlA.js","_app/immutable/entry/app.1zOPkDX9.js","_app/immutable/chunks/scheduler.BBaljRlA.js","_app/immutable/chunks/index.nkUJBPft.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
--- .svelte-kit/output/server/nodes/0.js
+++ .svelte-kit/output/server/nodes/0.js
@@ -3,6 +3,6 @@
 export const index = 0;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.DQYah_UC.js","_app/immutable/chunks/scheduler.BBaljRlA.js","_app/immutable/chunks/index.nkUJBPft.js","_app/immutable/chunks/constants.Bo-olcIz.js"];
-export const stylesheets = ["_app/immutable/assets/0.BYA30vKs.css"];
+export const imports = ["_app/immutable/nodes/0.CqrJ6GOO.js","_app/immutable/chunks/scheduler.BBaljRlA.js","_app/immutable/chunks/index.nkUJBPft.js","_app/immutable/chunks/constants.aT1qBy2V.js"];
+export const stylesheets = ["_app/immutable/assets/0.CwYusUqc.css"];
 export const fonts = [];
