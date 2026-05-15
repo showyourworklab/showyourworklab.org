ed323a529ae27be468574cdc1479dcdeeb693afa
Corey Tegeler <coreytegeler@gmail.com>
2026-05-15 17:14:03 -0400

    Replace with fresh Next.js install

--- src/helpers.js
+++
@@ -1,9 +0,0 @@
-export function formatDate(dateStr) {
-	const dateObj = new Date(dateStr);
-	const formattedDateStr = dateObj.toLocaleDateString("en-US", {
-		year: "numeric",
-		month: "long",
-		day: "numeric",
-	});
-	return formattedDateStr;
-}
